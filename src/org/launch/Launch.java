f5dce8808c777313c975bc88df1c97bb64dd2ee2
nithu0531 <s.nithaeshkumar@gmail.com>
2021-10-18 10:24:32 -0700

    Just uploading for sample

---
+++ src/org/launch/Launch.java
@@ -0,0 +1,15 @@
+package org.launch;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Launch {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nithaesh\\eclipse-workspace\\Selenium\\drivers\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://en-gb.facebook.com/");
+	}
+
+}
